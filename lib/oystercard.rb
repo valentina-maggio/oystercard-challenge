6d7d532183a2931144de702a47f264cab2be8a46
Jonny Abrams <jonnyabrams@hotmail.com>
2022-02-28 16:23:23 +0000

    Updated Oystercard class * Added top_up tests * Added top_up method which increments balance
    Co-authored-by: Paul Kilgarriff <PKilgarriff@users.noreply.github.com>
    Co-authored-by: Valentina Maggio <valentina-maggio@users.noreply.github.com>

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,13 @@
 class Oystercard
   DEFAULT_BALANCE = 0
 
-  attr_reader :balance 
+  attr_reader :balance
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
+  end
+
+  def top_up(money)
+    @balance += money
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,13 @@ describe Oystercard do
   it 'has an initial balance of 0' do
     expect(subject.balance).to be_zero
   end
+
+  it 'responds to top up method' do
+    expect(subject).to respond_to(:top_up)
+  end
+
+  it 'tops up the balance' do
+    # when you add 20 to the balance it returns 20 (the balance)
+    expect { subject.top_up(20) }.to change { subject.balance }.by(20)
+  end 
 end
